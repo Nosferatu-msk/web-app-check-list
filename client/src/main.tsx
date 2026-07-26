9fa76b6e6cf7aaecc7d542c96e9186349293bc91
Nosferatu-msk <nosferatu-msk@users.noreply.github.com>
2026-07-26 09:52:03 +0300

    fix: use ConfigProvider controlHeight token instead of CSS overrides for element sizing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,32 @@ import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider, App as AntApp } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import App from './App';
+import { useIsMobile } from './hooks/useIsMobile';
 import './index.css';
+
+function Providers({ children }: { children: React.ReactNode }) {
+  const isMobile = useIsMobile();
+  return (
+    <ConfigProvider
+      locale={ruRU}
+      theme={{
+        token: {
+          colorPrimary: '#1677ff',
+          controlHeight: isMobile ? 40 : 32,
+        },
+      }}
+    >
+      <AntApp>{children}</AntApp>
+    </ConfigProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ConfigProvider locale={ruRU} theme={{ token: { colorPrimary: '#1677ff' } }}>
-        <AntApp>
-          <App />
-        </AntApp>
-      </ConfigProvider>
+      <Providers>
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>,
 );
