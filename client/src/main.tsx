55bebdb47d870fd8687dae2c99d697f919032dd7
Nosferatu-msk <nosferatu-msk@users.noreply.github.com>
2026-07-26 09:58:15 +0300

    fix: set controlHeight: 40 globally via ConfigProvider token

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,32 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider, App as AntApp } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import App from './App';
-import { useIsMobile } from './hooks/useIsMobile';
 import './index.css';
-
-function Providers({ children }: { children: React.ReactNode }) {
-  const isMobile = useIsMobile();
-  return (
-    <ConfigProvider
-      locale={ruRU}
-      theme={{
-        token: {
-          colorPrimary: '#1677ff',
-          controlHeight: isMobile ? 40 : 32,
-        },
-      }}
-    >
-      <AntApp>{children}</AntApp>
-    </ConfigProvider>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Providers>
-        <App />
-      </Providers>
+      <ConfigProvider
+        locale={ruRU}
+        theme={{
+          token: {
+            colorPrimary: '#1677ff',
+            controlHeight: 40,
+          },
+        }}
+      >
+        <AntApp>
+          <App />
+        </AntApp>
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
